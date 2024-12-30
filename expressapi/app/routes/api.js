bc3ba62d2918dbfda0bae012172aa82d96ca1f14
Sallai András <andrewteki@gmail.com>
2024-12-30 08:27:53 +0100

    Fixed directory name in expressapi

--- expressapi/app/routes/api.js
+++ expressapi/app/routes/api.js
@@ -3,7 +3,7 @@ const router = Router()
 
 const AuthController = require('../controllers/authcontroller')
 const UserController = require('../controllers/usercontroller')
-const { verifyToken } = require('../middleware/authjwt')
+const { verifyToken } = require('../middlewares/authjwt')
  
 router.post('/register', AuthController.register)
 router.post('/login', AuthController.login)
