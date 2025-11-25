bc202dfe061b25a566b437911500a1d6b1b86608
Sallai András <andrewteki@gmail.com>
2025-11-25 16:17:01 +0100

    expressapi: Fixed seeder

--- expressapi/templates/seederTemplate.js
+++ expressapi/templates/seederTemplate.js
@@ -1,20 +1,12 @@
-import db from '../../app/models/modrels.js';
 
 async function up({context: QueryInterface}) {
-  if(db.Thing) {
-    await db.Thing.bulkCreate([
-      
-    ]);
-  }else {
-    await QueryInterface.bulkInsert('things', [
+  await QueryInterface.bulkInsert('things', [
 
-    ]);
-  }
-
+  ]);
 }
 
 async function down({context: QueryInterface}) {
-  await QueryInterface.bulkDelete('things');
+  await QueryInterface.bulkDelete('things', null, {});
 }
 
 export { up, down }
