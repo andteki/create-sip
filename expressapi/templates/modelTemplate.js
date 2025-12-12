ecf29a50b25e4f7b372da8a95c5c6d3a5038aa78
Sallai András <andrewteki@gmail.com>
2025-12-12 18:23:02 +0100

    expressapi: Deleted templates directory

--- expressapi/templates/modelTemplate.js
+++
@@ -1,8 +0,0 @@
-import { DataTypes } from 'sequelize'
-import sequelize from '../database/database.js'
-
-const Thing = sequelize.define('thing', {
-    name: { type: DataTypes.STRING,  allowNull: false  }
-})
-
-export default Thing
